df79a08079cab40bfe9b13235136d957f7a6c531
Juho Vepsalainen <bebraw@gmail.com>
2011-11-10 10:06:35 +0200

    Sketch out question types

--- src/pyqa.py
+++ src/pyqa.py
@@ -6,11 +6,30 @@ def load(source):
         return map(lambda a: a, yaml.load_all(f))
 
 def ask(questions):
-    def _ask(q):
-        print(q['q'])
-        # XXX: might want to give a nice error about missing id
-        # TODO: deal with matches, yesno and options
-        return {'a': raw_input(), 'id': q['id']}
+    def _fill(i):
+        def _f(o):
+            o['type'] = o.get('type', 'choice' if o.get('choices') else 'answer') 
+            o['id'] = o.get('id') # XXX: warn about missing id?
 
-    return map(_ask, questions)
+            return o
+
+        return map(_f, i)
+
+    def _ask(q):
+        def _choice():
+            map(lambda (i, c): print(str(i) + ': ' + c), enumerate(q['choices']))
+
+            # TODO: make sure input is within proper range
+            # TODO: deal with matches
+            return raw_input()
+
+        a = {
+            'answer': lambda: raw_input(),
+            'choice': _choice,
+            'yesno': lambda: raw_input().lower() in ('yes', '1', 'true'),
+        }[q['type']]()
+        
+        return {'a': a, 'id': q['id']}
+
+    return map(_ask, _fill(questions))
 
