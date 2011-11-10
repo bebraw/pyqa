19ce6528a722deec9f0080c229c329e15b843614
Juho Vepsalainen <bebraw@gmail.com>
2011-11-10 09:34:24 +0200

    Make it possible to load questions

--- src/pyqa.py
+++ src/pyqa.py
@@ -1,3 +1,10 @@
+from __future__ import with_statement
+import yaml
+
+def load_file(source):
+    with open(source) as f:
+        return map(lambda a: a, yaml.load_all(f))
+
 def main():
     pass
 
